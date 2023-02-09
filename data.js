f8e4326d7417b637e0acf86a3d3c2051eeadba65
KavyaCOder1 <97831532+KavyaCOder1@users.noreply.github.com>
2023-02-09 11:39:45 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,25 @@
+module.exports = {
+
+    mongoLink:'mongodb://kavya9109:kavya9109@ac-f7sytui-shard-00-00.vajav6l.mongodb.net:27017,ac-f7sytui-shard-00-01.vajav6l.mongodb.net:27017,ac-f7sytui-shard-00-02.vajav6l.mongodb.net:27017/?ssl=true&replicaSet=atlas-5z6f25-shard-0&authSource=admin&retryWrites=true&w=majority', // mongo-link from cloud.mongodb.com
+    
+    bot_token:'6123185830:AAGNK0J_3-ACDkaRdtKsrqxANrCwjY1BiZU',//bot token
+    
+    bot_name:'@celotest82_bot',//bot username without @
+    
+    bot_admin:5556717138,// admin telegram id
+    
+    channelsList : ['@AirdropSupremacy'],// channels list
+
+    twitter:'https://twitter.com/YourTwitter',//your twitter link
+    
+    reffer_bonus:0.001,// refferal bonus amount
+    
+    min_wd:0.003,//min withdrawal amount
+    
+    daily_bonus:0.001,// daily bonus amount
+    
+    currency:'CELO', //bot currency
+    
+    payment_channel:'@AirdropSupremacy'// payment channel username
+    
+    }
