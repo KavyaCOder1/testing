50fa1e7fb468d040af74d6dc692d62a66f95a20d
KavyaCOder1 <97831532+KavyaCOder1@users.noreply.github.com>
2023-02-11 10:37:26 +0530

    Update celo.js

--- celo.js
+++ celo.js
@@ -22,9 +22,9 @@ mongo.connect(data.mongoLink, {useUnifiedTopology: true}, (err, client) => {
 })
 })
 
-setInterval(
-	console.log('I am Alive'), 25000 
-);
+
+	 
+
 
 bot.use(session())
 bot.use(stage.middleware())
